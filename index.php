f90dd7b8b473f87272efe7e8bdb58475bc2007d7
Alicia Broadhurst <abroadhurst@cnm.edu>
2016-04-13 12:01:21 -0600

    Added index.php file

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8"/>
+		<title>About Me</title>
+	</head>
+	<body>
+		<h1>About Me</h1>
+		<h2>Something here?</h2>
+		<p>Text.	</p>
+		<h2>Another section?</h2>
+		<p>More text.</p>
+	</body>
+</html>
