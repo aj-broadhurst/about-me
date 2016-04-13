e11aaa0b1fc6723d2d5e5adbcda5e139a5d55310
Alicia Broadhurst <abroadhurst@cnm.edu>
2016-04-13 14:05:38 -0600

    Created Version .1 of the About Me site

--- index.php
+++ index.php
@@ -6,9 +6,27 @@
 	</head>
 	<body>
 		<h1>About Me</h1>
-		<h2>Something here?</h2>
-		<p>Text.	</p>
-		<h2>Another section?</h2>
-		<p>More text.</p>
+		<h2>A Brief Description</h2>
+		<p>My name is Alicia Broadhurst.  I am currently attending the Spring 2016 Deep Dive Coding Bootcamp. This is a
+		page that says a little bit about me.</p>
+		<h2>Some of My Hobbies</h2>
+		<p>Here are some of my hobbies.</p>
+			<ul>
+			<li>Rock Climbing</li>
+			<li>Swing Dancing</li>
+			<li>Video Games
+				<ul>
+				Some of my favorite genres:
+				<li>Rhythm Games</li>
+				<li>FPS</li>
+				<li>Puzzle Games</li>
+				</ul>
+			</li>
+			<li>Creative Writing</li>
+			<li>Photography</li>
+			</ul>
+		<p>Here is a photograph that I took:</p>
+		<img src="images/rattlesnake.jpg" alt="Photo of Black-Tailed Rattlesnake" height="394" width="700">
+		<p>Some of my favorite animals are reptiles or amphibians. I also really like dinosaurs, sharks, spiders, and bats.</p>
 	</body>
 </html>
